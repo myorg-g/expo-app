f4c231a0178f6f10b1e4a4575713c9b7002ba7c6
gudditinaganjaneyulu <gudditinaganjaneyulu@gmail.com>
2024-06-25 07:50:51 +0000

    test-3

--- app.config.js
+++ app.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     expo: {
-      owner: "your-expo-username-or-team-id",
+      owner: "gudditi",
       icon: process.env.APP_ICON || 'https://www.blackbox.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FBlackbox-Logo-4x.85cc4976.png&w=384&q=75',
-      name: process.env.APP_NAME || 'My App'
+      name: process.env.APP_NAME || 'gudditi-test'
     }
   };
   
