302c0419e744d4c81a7b9b570b5b301b9022dcfc
gudditinaganjaneyulu <gudditinaganjaneyulu@gmail.com>
2024-06-25 07:44:40 +0000

    test-2

--- app.config.js
+++ app.config.js
@@ -1,9 +1,8 @@
-{
-    "expo": {
-      "owner": "your-expo-username-or-team-id",
-      "icon": "process.env.APP_ICON || 'https://www.blackbox.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FBlackbox-Logo-4x.85cc4976.png&w=384&q=75'",
-      "name": "process.env.APP_NAME || 'My App'"
-      // other configuration properties
+module.exports = {
+    expo: {
+      owner: "your-expo-username-or-team-id",
+      icon: process.env.APP_ICON || 'https://www.blackbox.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FBlackbox-Logo-4x.85cc4976.png&w=384&q=75',
+      name: process.env.APP_NAME || 'My App'
     }
-  }
+  };
   
