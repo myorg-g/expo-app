198f705c1b0f4c712330073db5ac43a42b1b73eb
gudditinaganjaneyulu <gudditinaganjaneyulu@gmail.com>
2024-06-25 08:13:03 +0000

    test -3.1.1

--- app.config.js
+++ app.config.js
@@ -1,8 +1,18 @@
 module.exports = {
-    expo: {
-      owner: "gudditi",
-      icon: process.env.APP_ICON || 'https://www.blackbox.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FBlackbox-Logo-4x.85cc4976.png&w=384&q=75',
-      name: process.env.APP_NAME || 'gudditi-test'
+  expo: {
+    owner: "gudditi",
+    icon: process.env.APP_ICON || 'https://www.blackbox.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FBlackbox-Logo-4x.85cc4976.png&w=384&q=75',
+    name: process.env.APP_NAME || 'gudditi-test',
+    extra: {
+      eas: {
+        projectId: "1178e2a7-3f80-4832-805c-ef0c17c0baec"
+      }
+    },
+    updates: {
+      url: "https://u.expo.dev/1178e2a7-3f80-4832-805c-ef0c17c0baec"
+    },
+    runtimeVersion: {
+      policy: "appVersion"
     }
-  };
-  
+  }
+};
