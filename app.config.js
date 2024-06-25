53e2953bb36ee4aecfa6a2d3e55da8302c1be048
gudditinaganjaneyulu <gudditinaganjaneyulu@gmail.com>
2024-06-25 07:41:25 +0000

    test-2

--- app.config.js
+++ app.config.js
@@ -1,6 +1,9 @@
-module.exports = {
-    // use the variable if it's defined, otherwise use the fallback
-    icon: process.env.APP_ICON || 'https://www.blackbox.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FBlackbox-Logo-4x.85cc4976.png&w=384&q=75',
-    name: process.env.APP_NAME || 'My App',
-  };
+{
+    "expo": {
+      "owner": "your-expo-username-or-team-id",
+      "icon": "process.env.APP_ICON || 'https://www.blackbox.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FBlackbox-Logo-4x.85cc4976.png&w=384&q=75'",
+      "name": "process.env.APP_NAME || 'My App'"
+      // other configuration properties
+    }
+  }
   
