da307e46e02119944ab4dfdb6524f0abed4b0f89
gudditinaganjaneyulu <gudditinaganjaneyulu@gmail.com>
2024-06-25 09:07:09 +0000

    changes made 1.1 test

--- app.config.js
+++ app.config.js
@@ -8,6 +8,21 @@ module.exports = {
         projectId: "1178e2a7-3f80-4832-805c-ef0c17c0baec"
       }
     },
+    "ios": {
+      "supportsTablet": true
+    },
+    "android": {
+      "adaptiveIcon": {
+        "foregroundImage": "https://i.pinimg.com/736x/af/34/95/af3495b8efa1bb2cadd15751ab0f1410.jpg",
+        "backgroundColor": "#ffd600"
+      },
+      "package": "com.sniffynose"
+    },
+    "web": {
+      "bundler": "metro",
+      "output": "static",
+      "favicon": "./assets/images/favicon.png"
+    },
     updates: {
       url: "https://u.expo.dev/1178e2a7-3f80-4832-805c-ef0c17c0baec"
     },
