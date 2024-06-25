fd21e42c9a373729fba659cb1bad0185d20d44e1
gudditinaganjaneyulu <gudditinaganjaneyulu@gmail.com>
2024-06-25 07:37:01 +0000

    test-2

---
+++ app.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    // use the variable if it's defined, otherwise use the fallback
+    icon: process.env.APP_ICON || 'https://www.blackbox.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FBlackbox-Logo-4x.85cc4976.png&w=384&q=75',
+    name: process.env.APP_NAME || 'My App',
+  };
+  
